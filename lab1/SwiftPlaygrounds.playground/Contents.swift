b57b95ae2200356090cbe9940d19b2d397dd83ee
GodfatherSean <zehao.sean.huang@gmail.com>
2019-02-02 00:37:45 -0800

    lab1 done

--- lab1/SwiftPlaygrounds.playground/Contents.swift
+++ lab1/SwiftPlaygrounds.playground/Contents.swift
@@ -16,61 +16,108 @@ import Foundation
 // Implement the following functions:
 
 func tossCoin() -> String {
-    // YOUR CODE HERE
-    
-    return ""
+    return arc4random() / 2 == 0 ? "Heads" : "Tails"
 }
 
 func simulateTosses(numTosses n: Int) -> Int{
-    
-    // YOUR CODE HERE
-    
-    return -1
+    var cnt = 0
+    for i in 1...n {
+        if tossCoin() == "Heads" {
+            cnt += 1
+        }
+    }
+    return cnt
 }
 
 // Uncomment the following lines to run sanity check tests:
-/*
+
 tossCoin()  // Should return either "Heads" or "Tails"
 simulateTosses(numTosses: 50)   // Should return a number close to 25
-*/
+
 /*:
  ### Problem 2: Digits
 */
 //Implement the `digits` function:
 
-// YOUR CODE HERE
+func digits(ofNumber n: Int) -> [Int] {
+    if n < 10 {
+        return [n]
+    } else {
+        return digits(ofNumber: n / 10) + [n % 10]
+    }
+}
 
 // Uncomment the following lines to run sanity check tests:
-/*
- digits(123)  // returns [1, 2, 3]
- digits(0)    // returns [0]
-*/
+
+digits(ofNumber: 123)  // returns [1, 2, 3]
+digits(ofNumber: 0)    // returns [0]
+
 /*:
  ### Problem 3: Number Frequency
 */
 // Implement the `frequency` function:
 
-// YOUR CODE HERE
+func frequency(ofNumbers ns: [Int]) -> String {
+    var dict = Dictionary<Int, Int>()
+    for n in ns {
+        if let val = dict[n] {
+            dict[n] = val + 1
+        } else {
+            dict[n] = 1
+        }
+    }
+    var result = ""
+    for key in dict.keys.sorted(by: <) {
+        if let val = dict[key] {
+            result += "\(key):\(val), "
+        }
+    }
+    return result.count == 0 ? "" : String(result.prefix(result.count - 2))
+}
 
 // Uncomment the following lines to run sanity check tests:
-/*
+
 frequency(ofNumbers: [3, 1, 2, 3, 2, 1, 2, 3, 2])  // returns "1:2, 2:4, 3:3"
 frequency(ofNumbers: [1, 2]) // returns "1:1, 2:1"
 frequency(ofNumbers: []) // returns ""
-*/
+
 /*:
  ### Problem 4 (Optional): Roman Numerals to Integer
  */
 // Implement the `romanToInt` function:
 
-// YOUR CODE HERE
+func romanToInt(ofRoman s : String) -> Int {
+    var dict = Dictionary<Character, Int>()
+    dict["I"] = 1
+    dict["V"] = 5
+    dict["X"] = 10
+    dict["L"] = 50
+    dict["C"] = 100
+    dict["D"] = 500
+    dict["M"] = 1000
+    var ans = 0, p: Character = "0"
+    for c in s {
+        if let val = dict[c] {
+            ans += val
+        }
+        if ((p == "I" && (c == "V" || c == "X"))
+            || (p == "X" && (c == "L" || c == "C"))
+            || (p == "C" && (c == "D" || c == "M"))) {
+            if let val = dict[p] {
+                ans -= 2 * val
+            }
+        }
+        p = c
+    }
+    return ans
+}
 
 // Uncomment the following lines to run sanity check tests:
-/*
- romanToInt("III")      // returns 3
- romanToInt("LVIII")    // returns 58
- romanToInt("MCMXCIV")  // returns 1994
- */
+
+ romanToInt(ofRoman: "III")      // returns 3
+ romanToInt(ofRoman: "LVIII")    // returns 58
+ romanToInt(ofRoman: "MCMXCIV")  // returns 1994
+
 
 
 
