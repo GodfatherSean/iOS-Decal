44a15e5bdec85176e8bc73dcc74ab5930b86f136
Zehao Huang <1078351005@qq.com>
2019-02-14 16:58:51 -0800

    lec2

---
+++ firsrt-ios-app/firsrt-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firsrt-ios-app
+//
+//  Created by Zehao Huang on 2019/2/12.
+//  Copyright © 2019 Zehao Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ firsrt-ios-app/firsrt-ios-appTests/firsrt_ios_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  firsrt_ios_appTests.swift
+//  firsrt-ios-appTests
+//
+//  Created by Zehao Huang on 2019/2/12.
+//  Copyright © 2019 Zehao Huang. All rights reserved.
+//
+
+import XCTest
+@testable import firsrt_ios_app
+
+class firsrt_ios_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
