3c17eed47f5930a0e0d7c8b81f78886e13815d72
GodfatherSean <zehao.sean.huang@gmail.com>
2019-03-06 02:08:21 -0800

    lab06

---
+++ lab5/Snapchat/Snapchat/Controller/FeedCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCell.swift
+//  Snapchat
+//
+//  Created by 黄泽皓 on 2019/3/6.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var cellFeedLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
---
+++ lab5/Snapchat/Snapchat/Controller/ImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  imageCellController.swift
+//  Snapchat
+//
+//  Created by 黄泽皓 on 2019/3/5.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    var cellImageLabel: String!
+}
